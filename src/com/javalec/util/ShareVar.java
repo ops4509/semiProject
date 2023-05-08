c68052f8aa0a5ab13b983c57629b0c4cca49f7d0
ops4509 <75060611+ops4509@users.noreply.github.com>
2023-05-08 09:28:02 +0900

    Update ShareVar.java

--- src/com/javalec/util/ShareVar.java
+++ src/com/javalec/util/ShareVar.java
@@ -4,4 +4,5 @@ public class ShareVar {
 	public static final String DBName = "jdbc:mysql://127.0.0.1/shoesmarket?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
 	public static final String DBUser = "root";
 	public static final String DBPass = "qwer1234";
+	public static int filename = 0;
 }
